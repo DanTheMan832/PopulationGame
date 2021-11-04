d675b2ec932cdf2ad75e2bd8b88383aa5d5ba8b8
DanTheMan832 <63188310+DanTheMan832@users.noreply.github.com>
2021-11-04 10:29:32 +0000

    Add files via upload

---
+++ Game/script.js
@@ -0,0 +1,397 @@
+﻿var data=[['1', 'China', '1439323776', 'Asia'],
+['2', 'India', '1380004385', 'Asia'],
+['3', 'United States', '331002651', 'North America'],
+['4', 'Indonesia', '273523615', 'Asia'],
+['5', 'Pakistan', '220892340', 'Asia'],
+['6', 'Brazil', '212559417', 'Latin America'],
+['7', 'Nigeria', '206139589', 'Africa'],
+['8', 'Bangladesh', '164689383', 'Asia'],
+['9', 'Russia', '145934462', 'Europe'],
+['10', 'Mexico', '128932753', 'Latin America'],
+['11', 'Japan', '126476461', 'Asia'],
+['12', 'Ethiopia', '114963588', 'Africa'],
+['13', 'Philippines', '109581078', 'Asia'],
+['14', 'Egypt', '102334404', 'Africa'],
+['15', 'Vietnam', '97338579', 'Asia'],
+['16', 'DR Congo', '89561403', 'Africa'],
+['17', 'Turkey', '84339067', 'Asia'],
+['18', 'Iran', '83992949', 'Asia'],
+['19', 'Germany', '83783942', 'Europe'],
+['20', 'Thailand', '69799978', 'Asia'],
+['21', 'United Kingdom', '67886011', 'Europe'],
+['22', 'France', '65273511', 'Europe'],
+['23', 'Italy', '60461826', 'Europe'],
+['24', 'Tanzania', '59734218', 'Africa'],
+['25', 'South Africa', '59308690', 'Africa'],
+['26', 'Myanmar', '54409800', 'Asia'],
+['27', 'Kenya', '53771296', 'Africa'],
+['28', 'South Korea', '51269185', 'Asia'],
+['29', 'Colombia', '50882891', 'Latin America'],
+['30', 'Spain', '46754778', 'Europe'],
+['31', 'Uganda', '45741007', 'Africa'],
+['32', 'Argentina', '45195774', 'Latin America'],
+['33', 'Algeria', '43851044', 'Africa'],
+['34', 'Sudan', '43849260', 'Africa'],
+['35', 'Ukraine', '43733762', 'Europe'],
+['36', 'Iraq', '40222493', 'Asia'],
+['37', 'Afghanistan', '38928346', 'Asia'],
+['38', 'Poland', '37846611', 'Europe'],
+['39', 'Canada', '37742154', 'North America'],
+['40', 'Morocco', '36910560', 'Africa'],
+['41', 'Saudi Arabia', '34813871', 'Asia'],
+['42', 'Uzbekistan', '33469203', 'Asia'],
+['43', 'Peru', '32971854', 'Latin America'],
+['44', 'Angola', '32866272', 'Africa'],
+['45', 'Malaysia', '32365999', 'Asia'],
+['46', 'Mozambique', '31255435', 'Africa'],
+['47', 'Ghana', '31072940', 'Africa'],
+['48', 'Yemen', '29825964', 'Asia'],
+['49', 'Nepal', '29136808', 'Asia'],
+['50', 'Venezuela', '28435940', 'Latin America'],
+['51', 'Madagascar', '27691018', 'Africa'],
+['52', 'Cameroon', '26545863', 'Africa'],
+['53', "Côte d'Ivoire", '26378274', 'Africa'],
+['54', 'North Korea', '25778816', 'Asia'],
+['55', 'Australia', '25499884', 'Oceania'],
+['56', 'Niger', '24206644', 'Africa'],
+['57', 'Sri Lanka', '21413249', 'Asia'],
+['58', 'Burkina Faso', '20903273', 'Africa'],
+['59', 'Mali', '20250833', 'Africa'],
+['60', 'Romania', '19237691', 'Europe'],
+['61', 'Malawi', '19129952', 'Africa'],
+['62', 'Chile', '19116201', 'Latin America'],
+['63', 'Kazakhstan', '18776707', 'Asia'],
+['64', 'Zambia', '18383955', 'Africa'],
+['65', 'Guatemala', '17915568', 'Latin America'],
+['66', 'Ecuador', '17643054', 'Latin America'],
+['67', 'Syria', '17500658', 'Asia'],
+['68', 'Netherlands', '17134872', 'Europe'],
+['69', 'Senegal', '16743927', 'Africa'],
+['70', 'Cambodia', '16718965', 'Asia'],
+['71', 'Chad', '16425864', 'Africa'],
+['72', 'Somalia', '15893222', 'Africa'],
+['73', 'Zimbabwe', '14862924', 'Africa'],
+['74', 'Guinea', '13132795', 'Africa'],
+['75', 'Rwanda', '12952218', 'Africa'],
+['76', 'Benin', '12123200', 'Africa'],
+['77', 'Burundi', '11890784', 'Africa'],
+['78', 'Tunisia', '11818619', 'Africa'],
+['79', 'Bolivia', '11673021', 'Latin America'],
+['80', 'Belgium', '11589623', 'Europe'],
+['81', 'Haiti', '11402528', 'Latin America'],
+['82', 'Cuba', '11326616', 'Latin America'],
+['83', 'South Sudan', '11193725', 'Africa'],
+['84', 'Dominican Republic', '10847910', 'Latin America'],
+['85', 'Czechia', '10708981', 'Europe'],
+['86', 'Greece', '10423054', 'Europe'],
+['87', 'Jordan', '10203134', 'Asia'],
+['88', 'Portugal', '10196709', 'Europe'],
+['89', 'Azerbaijan', '10139177', 'Asia'],
+['90', 'Sweden', '10099265', 'Europe'],
+['91', 'Honduras', '9904607', 'Latin America'],
+['92', 'United Arab Emirates', '9890402', 'Asia'],
+['93', 'Hungary', '9660351', 'Europe'],
+['94', 'Tajikistan', '9537645', 'Asia'],
+['95', 'Belarus', '9449323', 'Europe'],
+['96', 'Austria', '9006398', 'Europe'],
+['97', 'Papua New Guinea', '8947024', 'Oceania'],
+['98', 'Serbia', '8737371', 'Europe'],
+['99', 'Israel', '8655535', 'Asia'],
+['100', 'Switzerland', '8654622', 'Europe'],
+['101', 'Togo', '8278724', 'Africa'],
+['102', 'Sierra Leone', '7976983', 'Africa'],
+['103', 'Laos', '7275560', 'Asia'],
+['104', 'Paraguay', '7132538', 'Latin America'],
+['105', 'Bulgaria', '6948445', 'Europe'],
+['106', 'Libya', '6871292', 'Africa'],
+['107', 'Lebanon', '6825445', 'Asia'],
+['108', 'Nicaragua', '6624554', 'Latin America'],
+['109', 'Kyrgyzstan', '6524195', 'Asia'],
+['110', 'El Salvador', '6486205', 'Latin America'],
+['111', 'Turkmenistan', '6031200', 'Asia'],
+['112', 'Singapore', '5850342', 'Asia'],
+['113', 'Denmark', '5792202', 'Europe'],
+['114', 'Finland', '5540720', 'Europe'],
+['115', 'Congo', '5518087', 'Africa'],
+['116', 'Slovakia', '5459642', 'Europe'],
+['117', 'Norway', '5421241', 'Europe'],
+['118', 'Oman', '5106626', 'Asia'],
+['119', 'State of Palestine', '5101414', 'Asia'],
+['120', 'Costa Rica', '5094118', 'Latin America'],
+['121', 'Liberia', '5057681', 'Africa'],
+['122', 'Ireland', '4937786', 'Europe'],
+['123', 'Central African Republic', '4829767', 'Africa'],
+['124', 'New Zealand', '4822233', 'Oceania'],
+['125', 'Mauritania', '4649658', 'Africa'],
+['126', 'Panama', '4314767', 'Latin America'],
+['127', 'Kuwait', '4270571', 'Asia'],
+['128', 'Croatia', '4105267', 'Europe'],
+['129', 'Moldova', '4033963', 'Europe'],
+['130', 'Georgia', '3989167', 'Asia'],
+['131', 'Eritrea', '3546421', 'Africa'],
+['132', 'Uruguay', '3473730', 'Latin America'],
+['133', 'Bosnia and Herzegovina', '3280819', 'Europe'],
+['134', 'Mongolia', '3278290', 'Asia'],
+['135', 'Armenia', '2963243', 'Asia'],
+['136', 'Jamaica', '2961167', 'Latin America'],
+['137', 'Qatar', '2881053', 'Asia'],
+['138', 'Albania', '2877797', 'Europe'],
+['139', 'Lithuania', '2722289', 'Europe'],
+['140', 'Namibia', '2540905', 'Africa'],
+['141', 'Gambia', '2416668', 'Africa'],
+['142', 'Botswana', '2351627', 'Africa'],
+['143', 'Gabon', '2225734', 'Africa'],
+['144', 'Lesotho', '2142249', 'Africa'],
+['145', 'North Macedonia', '2083374', 'Europe'],
+['146', 'Slovenia', '2078938', 'Europe'],
+['147', 'Guinea-Bissau', '1968001', 'Africa'],
+['148', 'Latvia', '1886198', 'Europe'],
+['149', 'Bahrain', '1701575', 'Asia'],
+['150', 'Equatorial Guinea', '1402985', 'Africa'],
+['151', 'Trinidad and Tobago', '1399488', 'Latin America'],
+['152', 'Estonia', '1326535', 'Europe'],
+['153', 'Timor-Leste', '1318445', 'Asia'],
+['154', 'Mauritius', '1271768', 'Africa'],
+['155', 'Cyprus', '1207359', 'Asia'],
+['156', 'Eswatini', '1160164', 'Africa'],
+['157', 'Djibouti', '988000', 'Africa'],
+['158', 'Fiji', '896445', 'Oceania'],
+['159', 'Comoros', '869601', 'Africa'],
+['160', 'Guyana', '786552', 'Latin America'],
+['161', 'Bhutan', '771608', 'Asia'],
+['162', 'Solomon Islands', '686884', 'Oceania'],
+['163', 'Montenegro', '628066', 'Europe'],
+['164', 'Luxembourg', '625978', 'Europe'],
+['165', 'Suriname', '586632', 'Latin America'],
+['166', 'Cabo Verde', '555987', 'Africa'],
+['167', 'Micronesia', '548914', 'Oceania'],
+['168', 'Maldives', '540544', 'Asia'],
+['169', 'Malta', '441543', 'Europe'],
+['170', 'Brunei', '437479', 'Asia'],
+['171', 'Belize', '397628', 'Latin America'],
+['172', 'Bahamas', '393244', 'Latin America'],
+['173', 'Iceland', '341243', 'Europe'],
+['174', 'Vanuatu', '307145', 'Oceania'],
+['175', 'Barbados', '287375', 'Latin America'],
+['176', 'Sao Tome & Principe', '219159', 'Africa'],
+['177', 'Samoa', '198414', 'Oceania'],
+['178', 'Saint Lucia', '183627', 'Latin America'],
+['179', 'Kiribati', '119449', 'Oceania'],
+['180', 'Grenada', '112523', 'Latin America'],
+['181', 'St. Vincent & Grenadines', '110940', 'Latin America'],
+['182', 'Tonga', '105695', 'Oceania'],
+['183', 'Seychelles', '98347', 'Africa'],
+['184', 'Antigua and Barbuda', '97929', 'Latin America'],
+['185', 'Andorra', '77265', 'Europe'],
+['186', 'Dominica', '71986', 'Latin America'],
+['187', 'Marshall Islands', '59190', 'Oceania'],
+['188', 'Saint Kitts & Nevis', '53199', 'Latin America'],
+['189', 'Monaco', '39242', 'Europe'],
+['190', 'Liechtenstein', '38128', 'Europe'],
+['191', 'San Marino', '33931', 'Europe'],
+['192', 'Palau', '18094', 'Oceania'],
+['193', 'Tuvalu', '11792', 'Oceania'],
+['194', 'Nauru', '10824', 'Oceania']]
+
+var imgURLS=["https://res.cloudinary.com/dazy8pszw/image/upload/v1634501059/popGame/trump_ddqnjx.jpg",
+"https://res.cloudinary.com/dazy8pszw/image/upload/v1634501060/popGame/biden_fr0hif.jpg",
+"https://res.cloudinary.com/dazy8pszw/image/upload/v1634501059/popGame/may_ul7zb8.jpg",
+"https://res.cloudinary.com/dazy8pszw/image/upload/v1634501059/popGame/kim_eqciic.jpg",
+"https://res.cloudinary.com/dazy8pszw/image/upload/v1634501059/popGame/queen_d597uo.jpg",
+"https://res.cloudinary.com/dazy8pszw/image/upload/v1634501061/popGame/boris_nspt9a.jpg",
+"https://res.cloudinary.com/dazy8pszw/image/upload/v1634502876/popGame/merkel_kmkwxq.jpg"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var animTime=1;
+var correct=document.getElementById("correct")
+var incorrect=document.getElementById("incorrect")
+var currImage=-1
+var score=0
+scoreText=document.getElementById("score")
+var playAgainBtn=document.getElementById("playAgain")
+playAgainBtn.addEventListener("click",function(){
+    showGame();
+
+},false)
+var button1=document.getElementById("option1")
+button1.addEventListener("click",function(){
+    chooseOption("1");
+
+},false)
+var button2=document.getElementById("option2")
+button2.addEventListener("click",function(){
+    chooseOption("2");
+},false)
+var finalScoreText=document.getElementById("finalScore")
+var timer=document.getElementById("timer")
+var funnyImage=document.getElementById("funnyImage")
+
+function chooseOption(opt){
+    let pop1=parseInt(b1status[2])
+    let pop2=parseInt(b2status[2])
+   
+    if((opt=="1"&&pop1>=pop2)||(opt=="2"&&pop2>=pop1)){
+        score+=1
+        scoreText.innerHTML="Score: "+score.toString();
+        setQuestion(data,button1,button2)
+	
+	correct.play()
+	stopTimer()
+	
+	startTimer()
+	pickImage()
+    
+    }
+    else{
+		incorrect.play()
+	showEnd()
+	
+	alert("You lose!")
+	
+	
+    }
+
+}
+
+function pickImage(){
+	
+	rand=Math.random()
+	
+	randomImageIndex=Math.floor(rand*imgURLS.length)
+	
+	while(randomImageIndex==currImage){
+		rand=Math.random()
+		randomImageIndex=Math.floor(rand*imgURLS.length)
+		
+	}
+	
+	currImage=randomImageIndex
+	funnyImage.src=imgURLS[randomImageIndex]
+}
+function showEnd(){
+	
+	stopTimer()
+	pickImage()
+	timer.style.visibility="hidden"
+	finalScoreText.innerHTML="FINAL SCORE: "+score.toString()
+	button1.style.visibility="hidden"
+	button2.style.visibility="hidden"
+	scoreText.style.visibility="hidden"
+	playAgainBtn.style.visibility="visible"
+}
+function showGame(){
+	startTimer()
+	timer.style.visibility="visible"
+	setQuestion(data,button1,button2)
+	score=0
+	scoreText.innerHTML="Score: "+score.toString();
+	finalScoreText.innerHTML=""
+	button1.style.visibility="visible"
+	button2.style.visibility="visible"
+	scoreText.style.visibility="visible"
+	playAgainBtn.style.visibility="hidden"
+}
+
+var b1status=[]
+var b2status=[]
+function setQuestion(data,b1,b2){
+    var total=data.length
+    random1=Math.floor(Math.random()*total)
+    random2=Math.floor(Math.random()*total)
+    while(random1==random2){
+    random2=Math.floor(Math.random()*total)
+    }	
+    let randomElement1 = data[random1]
+    let randomElement2 = data[random2]
+    b1.innerHTML=randomElement1[1]
+    b2.innerHTML=randomElement2[1]
+    b1status=randomElement1
+    b2status=randomElement2
+}
+setQuestion(data,button1,button2)
+var timeRemaining=-1
+var interval
+startTimer()
+
+
+
+
+
+function startTimer(){
+	
+	timeRemaining=5
+	timer.innerHTML="Time Remaining: "+timeRemaining.toString();
+	interval=setInterval(function(){
+		timerInterval()
+	},1000);
+}
+function stopTimer(){
+	clearInterval(interval)
+}
+function timerInterval(){
+	
+	timeRemaining=timeRemaining-1
+	timer.innerHTML="Time Remaining: "+timeRemaining.toString();
+	if (timeRemaining<=0){
+	timer.innerHTML="Time Up!"
+	showEnd()
+	clearInterval(interval)
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
